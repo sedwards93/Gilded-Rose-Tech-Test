8e56fd8a493db2449fd3fe0faf27a30a50764728
Symion Edwards <symion.edwards@gmail.com>
2021-01-13 14:52:22 +0000

    Commit before trying to separate out items into classes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,6 +15,25 @@ class GildedRose
     end
   end
 
+  def update_item(item)
+    decrease_sell_in(item)
+    case item.name
+    when 'Aged Brie'
+      increase_quality(item)
+      increase_quality(item) if item.sell_in < 1
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      increase_quality(item)
+      increase_quality(item) if item.sell_in < 10
+      increase_quality(item) if item.sell_in < 6
+      item.quality -= item.quality if item.sell_in < 1
+    else
+      decrease_qaulity(item)
+      decrease_qaulity(item) if item.sell_in < 1
+    end
+  end
+
+  private
+  
   def increase_quality(item)
     item.quality += 1 if item.quality < 50
   end
@@ -27,22 +46,4 @@ class GildedRose
     item.sell_in -= 1 unless item.name == 'Sulfuras, Hand of Ragnaros'
   end
 
-  def update_item(item)
-    case item.name
-    when 'Aged Brie'
-      increase_quality(item)
-      increase_quality(item) if item.sell_in < 1
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      increase_quality(item)
-      increase_quality(item) if item.sell_in < 11
-      increase_quality(item) if item.sell_in < 6
-      item.quality -= item.quality if item.sell_in < 1
-    else
-      decrease_qaulity(item)
-      decrease_qaulity(item) if item.sell_in < 1
-    end
-
-    decrease_sell_in(item)
-
-  end
 end
